6cda503085b5f29e7034a140ec09a418b43f3df4
Soma Mbadiwe <somasystemsng@gmail.com>
2019-04-29 00:20:13 -0400

    Working on visualizing queries

--- main.py
+++ main.py
@@ -237,7 +237,7 @@ def evaluate(session, model, dataloader, cfg):
         plt.legend()
         plt.show()  # savefig('name2d.png')
 
-    return MAPs(cfg.DATA.MAP_R).get_maps_by_feature(database=db, query=test)
+    return MAPs(cfg.DATA.MAP_R).get_maps_by_feature(database=db, query=test, dataloader=dataloader)
 
 
 def plot_sample_data(df, feat_cols, rndperm):
